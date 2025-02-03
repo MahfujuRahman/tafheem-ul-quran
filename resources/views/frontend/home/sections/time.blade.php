718cc9db503ed44cd3e19005db343548db78faa9
MahfujuRahman <mahfujur15-14276@diu.edu.bd>
2025-02-04 00:26:34 +0600

    Initital Roja Time and Prayer Section Design

--- resources/views/frontend/home/sections/time.blade.php
+++ resources/views/frontend/home/sections/time.blade.php
@@ -1,95 +1,174 @@
 <section>
     <div class="w-100 tq-pt-120 tq-pb-110 position-relative">
         <div class="container">
-            <div
-                class="tq-sec-title-wrapper d-flex flex-column align-items-center text-center position-relative w-100">
+            <div class="tq-sec-title-wrapper d-flex flex-column align-items-center text-center position-relative w-100">
                 <div class="tq-sec-title d-flex flex-column align-items-center text-center">
-                    <img decoding="async" class="img-fluid" src="../frontend/plugins/ingeniofy-plus/assets/images/shape2.png" alt="Together We Prosper The" loading="lazy">
+                    <img decoding="async" class="img-fluid"
+                        src="../frontend/plugins/ingeniofy-plus/assets/images/shape2.png" alt="Together We Prosper The"
+                        loading="lazy">
                     <span>Welcome to the Islamic center</span>
                     <h2 class="mb-0">Together We Prosper The <br> House Of Allah</h2>
                 </div><!-- Section Title -->
             </div><!-- Section Title Wrapper -->
             <div class="tq-about-wrapper position-relative w-100">
                 <div class="row">
-                    <div class="col-md-12 col-sm-12 col-lg-7 order-lg-1">
-                        <div
-                            class="tq-about-img-wrap d-flex align-items-start justify-content-end position-relative w-100">
-                            <div class="tq-about-img2 overflow-hidden w-100"><img decoding="async" class="img-fluid w-100"
-                                    src="../frontend/uploads/2023/12/tq-service-img2.png" alt="Together We Prosper The">
-                                    <svg class="fill-theme position-absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1285 519">
-                                        <path d="M0,260c0-0.33,0-0.67,0-1c9.42-8.17,16.57-18.17,23.03-28.7c7.75-12.65,14-26.11,20.85-39.24
-                                            c4.55-8.73,9.51-17.25,16.28-24.46c8.71-9.26,18.57-16.23,32.36-14.39c13.51,1.8,21.11-4.01,22.46-17.52
-                                            c4.13-41.25,33.38-72.03,76.91-68.94c9.58,0.68,14.59-3.27,18.46-12.91c5.49-13.7,13.5-25.49,24.97-34.96
-                                            C247.23,8.05,261.11,2.93,276,0c244.67,0,489.33,0,734,0c1.77,0.45,3.53,0.95,5.31,1.34c11.88,2.6,22.79,7.34,32.48,14.75
-                                            c12.35,9.44,21.19,21.49,26.92,35.92c4.38,11.05,8.94,13.95,20.92,13.83c1.5-0.02,3-0.07,4.5-0.04
-                                            c27.56,0.5,48.13,12.51,61.02,37.12c5.16,9.86,8.19,20.32,9.13,31.44c1.08,12.78,8.95,19.93,20.75,18.06
-                                            c11.81-1.87,21.22,2.23,29.7,9.73c5.4,4.78,9.81,10.39,13.67,16.48c6.7,10.58,11.92,21.95,17.64,33.04
-                                            c8.89,17.23,18.69,33.83,32.96,47.32c0,0.33,0,0.67,0,1c-8.91,8.63-16.36,18.4-22.78,28.98c-7.7,12.69-13.89,26.18-20.83,39.27
-                                            c-5.32,10.03-11.08,19.76-19.53,27.59c-8.86,8.21-18.78,13.16-31.29,10.65c-2.14-0.43-4.3-0.22-6.44,0.29
-                                            c-8.8,2.09-12.78,7.93-14.02,19.37c-3.97,36.63-29.61,68.71-73.45,67.09c-13.35-0.49-17.26,2.31-22.32,14.69
-                                            c-6.15,15.03-15.69,27.3-28.95,36.76c-10.68,7.62-22.77,11.56-35.38,14.3c-245,0-490,0-735,0c-11.35-2.76-22.45-6.05-32.41-12.56
-                                            c-15.04-9.82-25.41-23.24-32.04-39.79c-4.23-10.56-8.93-13.58-20.16-13.49c-2.66,0.02-5.33,0.11-7.99,0.03
-                                            c-17.38-0.56-32.57-6.6-44.92-18.89c-14.07-14.01-20.68-31.47-22.59-50.98c-1.15-11.71-9.39-18.43-20.87-16.67
-                                            c-9.92,1.52-18.38-1.21-26.16-7.12c-10.36-7.88-16.99-18.64-23.02-29.81c-5.45-10.11-10.46-20.46-15.88-30.59
-                                            C21.23,284.68,12.39,271.02,0,260z"></path>
-                                    </svg>
+
+                    <div class="col-12 col-lg-5 order-lg-1 prayer-time-section">
+                        <div class="tq-about-img-wrap d-flex align-items-center justify-content-center position-relative w-100">
+                            <div class="tq-about-img2 overflow-hidden w-100 text-center">
+                                <img decoding="async" class="img-fluid w-100" src="../frontend/uploads/2023/12/tq-service-img2.png" alt="Together We Prosper">
+                            </div>
+                        </div>
+                        <div class="prayer-card position-relative overflow-hidden">
+                            <div class="prayer-gradient-bg"></div>
+                            <div class="prayer-content position-relative">
+                                <div class="prayer-header">
+                                    <h2 class="prayer-title">Prayer Times <i class="fas fa-praying-hands ms-2"></i></h2>
+                                    <div class="prayer-subtitle">Today's Schedule</div>
                                 </div>
-                            <!-- About Image -->
-                            <div class="tq-namaz-timing-box2 position-relative">
-                                <svg class="fill-theme"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 184 281">
-                                <g>
-                                    <path
-                                        d="M184,72c0,45.67,0,91.33,0,137c-3.3,4.84-7.56,8.35-13.42,9.66c-2.18,0.49-3.38,1.86-3.81,4.08
-                                        c-2.43,12.48-11.13,17.35-22.76,18.52c-2.09,0.21-4.01,0.61-5.04,2.62c-5.68,11.04-15.46,16.87-26.43,21.58
-                                        C104.74,268.8,97.36,273.12,93,281c-0.33,0-0.67,0-1,0c-5.44-8.58-13.89-13.21-22.85-16.97c-9.31-3.91-17.25-9.39-22.27-18.33
-                                        c-1.76-3.14-3.91-4.36-7.49-4.54c-12.32-0.6-19.47-6.63-22.47-18.67c-0.51-2.04-1.41-3.36-3.43-3.73C7.77,217.7,3.57,214.35,0,210
-                                        c0-46.33,0-92.67,0-139c3.57-4.35,7.77-7.7,13.5-8.76c1.75-0.32,2.78-1.38,3.34-3.24c4.15-13.84,9.21-17.89,24.05-19.28
-                                        c2.14-0.2,3.87-0.73,4.93-2.74c5.5-10.43,14.54-16.43,25.15-20.84C79.31,12.67,87,8,92,0c0.33,0,0.67,0,1,0
-                                        c5.02,8.78,13.53,13.02,22.26,16.8c9.55,4.14,17.87,9.63,22.88,19.07c1.53,2.88,3.8,3.71,6.87,3.97
-                                        c11.74,1.01,18.55,6.68,21.6,17.95c0.69,2.54,1.86,4.14,4.45,4.71C176.75,63.77,180.78,67.35,184,72z" />
-                                </g>
-                                </svg>
-                                <div
-                                    class="namaz-timing-box-inner position-absolute d-flex flex-column align-items-center">
-                                    <h3 class="mb-0">Prayer Time</h3>
-                                    <ul class="d-flex flex-column mb-0 list-unstyled">
-                                        <li><span>Fajr</span> ……………………….. <span>04:45 am</span></li>
-                                        <li><span>Dhuhr</span> ……………………. <span>01:30 am</span></li>
-                                        <li><span>Asr</span> ………………………… <span>06:45 am</span></li>
-                                        <li><span>Maghrib</span> ……………….. <span>05:53 pm</span></li>
-                                        <li><span>Isha</span> ………………………. <span>08:04 pm</span></li>
-                                        <li><span>Jummah</span> ………………. <span>04:45 pm</span></li>
-                                    </ul>
-                                </div><!-- Namaz Timing Box Inner -->
-                            </div><!-- Namaz Timing Box 2 -->
-                        </div><!-- About Image Wrap -->
+                                <div class="prayer-times-container">
+                                    <div class="prayer-item fajr">
+                                        <i class="prayer-icon fas fa-sun"></i>
+                                        <div class="prayer-info">
+                                            <span class="prayer-name">Fajr</span>
+                                            <span class="prayer-time">04:45 AM</span>
+                                        </div>
+                                    </div>
+                                    <div class="prayer-item dhuhr">
+                                        <i class="prayer-icon fas fa-sun"></i>
+                                        <div class="prayer-info">
+                                            <span class="prayer-name">Dhuhr</span>
+                                            <span class="prayer-time">01:30 PM</span>
+                                        </div>
+                                    </div>
+                                    <div class="prayer-item asr">
+                                        <i class="prayer-icon fas fa-sun"></i>
+                                        <div class="prayer-info">
+                                            <span class="prayer-name">Asr</span>
+                                            <span class="prayer-time">06:45 PM</span>
+                                        </div>
+                                    </div>
+                                    <div class="prayer-item maghrib">
+                                        <i class="prayer-icon fas fa-moon"></i>
+                                        <div class="prayer-info">
+                                            <span class="prayer-name">Maghrib</span>
+                                            <span class="prayer-time">05:53 PM</span>
+                                        </div>
+                                    </div>
+                                    <div class="prayer-item isha">
+                                        <i class="prayer-icon fas fa-moon"></i>
+                                        <div class="prayer-info">
+                                            <span class="prayer-name">Isha</span>
+                                            <span class="prayer-time">08:04 PM</span>
+                                        </div>
+                                    </div>
+                                    <div class="prayer-item jummah">
+                                        <i class="prayer-icon fas fa-mosque"></i>
+                                        <div class="prayer-info">
+                                            <span class="prayer-name">Jummah</span>
+                                            <span class="prayer-time">01:30 PM</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
 
-                    
-                    <div class="col-md-12 col-sm-12 col-lg-5">
-                        <div class="tq-about-cap tq-mt-60 d-flex flex-column align-items-start w-100">
+                    <div class="col-md-12 col-sm-12 col-lg-7 roja_left">
+                        <div
+                            class="tq-about-cap margin-top-30 d-flex flex-column align-items-center w-100 roja_timming">
                             <h3 class="mb-0">Roja Timing</h3>
-                            <ul class="d-flex flex-column mb-0 list-unstyled">
-                                <li><span>Sehri</span> ……………………….. <span>04:00 am</span></li>
-                                <li><span>Iftar</span> ……………………….. <span>06:30 pm</span></li>
-                            </ul>
+                            <div class="d-flex flex-row gap-5 justify-content-between w-100 mb-0 list-unstyled">
+                                <div>
+                                    <strong>Today</strong> Sehri: 04:00 am
+                                </div>
+                                <div>
+                                    <strong>Today</strong> Iftar: 06:30 pm
+                                </div>
+                            </div>
                         </div><!-- Roja Timing -->
-                        <div class="tq-about-cap tq-mt-60 d-flex flex-column align-items-start w-100">
-                            <h3 class="mb-0">30 Days Ramadan Timing</h3>
-                            <ul class="d-flex flex-column mb-0 list-unstyled" style="max-height: 300px; overflow-y: auto;">
-                                @for ($day = 1; $day <= 30; $day++)
-                                    <li>
-                                        <span class="day-number">Day {{ $day }}</span>
-                                        <div class="timing-details">
-                                            <span class="sehri-time">Sehri: 04:00 am</span>
-                                            <span class="iftar-time">Iftar: 06:30 pm</span>
+                        <div class="col-lg-12">
+                            <div class="roja_tables p-3 shadow-sm">
+                                <h3 class="roja_heading text-center mb-3">30 Days Ramadan Timing</h3>
+                                <div class="d-flex flex-wrap justify-content-between gap-3">
+                                    <!-- First 10 days -->
+                                    <div class="timing-section">
+                                        <h4 class="section-title">Days 1-10</h4>
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered text-center">
+                                                <thead class="table-dark">
+                                                    <tr>
+                                                        <th>Day</th>
+                                                        <th>Sehri</th>
+                                                        <th>Iftar</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @for ($day = 1; $day <= 10; $day++)
+                                                        <tr>
+                                                            <td>Day {{ $day }}</td>
+                                                            <td>04:00 AM</td>
+                                                            <td>06:30 PM</td>
+                                                        </tr>
+                                                    @endfor
+                                                </tbody>
+                                            </table>
                                         </div>
-                                    </li>
-                                @endfor
-                            </ul>
-                        </div><!-- 30 Days Ramadan Timing -->
+                                    </div>
+
+                                    <!-- Second 10 days -->
+                                    <div class="timing-section">
+                                        <h4 class="section-title">Days 11-20</h4>
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered text-center">
+                                                <thead class="table-dark">
+                                                    <tr>
+                                                        <th>Day</th>
+                                                        <th>Sehri</th>
+                                                        <th>Iftar</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @for ($day = 11; $day <= 20; $day++)
+                                                        <tr>
+                                                            <td>Day {{ $day }}</td>
+                                                            <td>04:00 AM</td>
+                                                            <td>06:30 PM</td>
+                                                        </tr>
+                                                    @endfor
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+
+                                    <!-- Last 10 days -->
+                                    <div class="timing-section">
+                                        <h4 class="section-title">Days 21-30</h4>
+                                        <div class="table-responsive">
+                                            <table class="table table-bordered text-center">
+                                                <thead class="table-dark">
+                                                    <tr>
+                                                        <th>Day</th>
+                                                        <th>Sehri</th>
+                                                        <th>Iftar</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @for ($day = 21; $day <= 30; $day++)
+                                                        <tr>
+                                                            <td>Day {{ $day }}</td>
+                                                            <td>04:00 AM</td>
+                                                            <td>06:30 PM</td>
+                                                        </tr>
+                                                    @endfor
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
 
@@ -99,3 +178,41 @@
         </div>
     </div>
 </section>
+
+<style>
+    .roja_tables {
+        background: #fff;
+        border-radius: 10px;
+        border: 1px solid #dee2e6;
+        padding: 20px;
+        transition: 0.3s ease-in-out;
+    }
+
+    .roja_tables:hover {
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    }
+
+    .roja_heading {
+        background: #28a745;
+        color: white;
+        padding: 12px;
+        border-radius: 5px;
+    }
+
+    .table th,
+    .table td {
+        vertical-align: middle;
+        font-size: 16px;
+    }
+
+    .table thead th {
+        background: #343a40 !important;
+        color: white;
+    }
+
+    @media (max-width: 768px) {
+        .roja_heading {
+            font-size: 20px;
+        }
+    }
+</style>
